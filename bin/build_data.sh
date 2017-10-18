d03d3bb2fe4cec0a41b84caa5998e0285c1f8c21
RouxRC <b.ooghe@gmail.com>
2017-10-18 06:10:00 +0200

    build data for filters

--- bin/build_data.sh
+++ bin/build_data.sh
@@ -9,64 +9,16 @@ function query {
 
 mkdir -p data
 
-query data/total_groupes "
-SELECT date, parlementaire_groupe_acronyme as groupes, sum(nb_mots) as total
-FROM intervention
-WHERE parlementaire_groupe_acronyme IS NOT NULL
-GROUP BY date, parlementaire_groupe_acronyme"
-
-query data/commissions_groupes "
-SELECT date, parlementaire_groupe_acronyme as groupes, sum(nb_mots) as total
-FROM intervention
-WHERE parlementaire_groupe_acronyme IS NOT NULL
-AND type = \"commission\"
-GROUP BY date, parlementaire_groupe_acronyme"
-
-query data/hemicycle_groupes "
-SELECT date, parlementaire_groupe_acronyme as groupes, sum(nb_mots) as total
-FROM intervention
-WHERE parlementaire_groupe_acronyme IS NOT NULL
-AND type <> \"commission\"
-GROUP BY date, parlementaire_groupe_acronyme"
-
-query data/total_genre "
-SELECT i.date, IF(p.sexe = \"H\", \"Hommes\", \"Femmes\") as genre, sum(i.nb_mots) as total
+query data/parole "
+SELECT
+  i.date,
+  i.parlementaire_groupe_acronyme as groupes,
+  p.sexe as genre,
+  IF(p.url_ancien_cpc IS NULL, \"nouveaux\", \"anciens\") as renouveau,
+  IF(i.type = \"commission\", \"commissions\", \"hemicycle\") as debats,
+  IF(i.nb_mots > 20, \"longues\", \"invectives\") as interventions,
+  sum(i.nb_mots) as total
 FROM intervention i
 JOIN parlementaire p ON p.id = i.parlementaire_id
-GROUP BY i.date, p.sexe"
+GROUP BY i.date, groupes, genre, renouveau, debats, interventions"
 
-query data/commissions_genre "
-SELECT i.date, IF(p.sexe = \"H\", \"Hommes\", \"Femmes\") as genre, sum(i.nb_mots) as total
-FROM intervention i
-JOIN parlementaire p ON p.id = i.parlementaire_id
-WHERE i.type = \"commission\"
-GROUP BY i.date, p.sexe"
-
-query data/hemicycle_genre "
-SELECT i.date, IF(p.sexe = \"H\", \"Hommes\", \"Femmes\") as genre, sum(i.nb_mots) as total
-FROM intervention i
-JOIN parlementaire p ON p.id = i.parlementaire_id
-WHERE i.type <> \"commission\"
-GROUP BY i.date, p.sexe"
-
-query data/total_renouveau "
-SELECT i.date, IF(url_ancien_cpc IS NULL, \"Nouveaux députés\", \"Députés réélus\") as renouveau, sum(i.nb_mots) as total
-FROM intervention i
-JOIN parlementaire p ON p.id = i.parlementaire_id
-GROUP BY i.date, renouveau"
-
-query data/commissions_renouveau "
-SELECT i.date, IF(url_ancien_cpc IS NULL, \"Nouveaux députés\", \"Députés réélus\") as renouveau, sum(i.nb_mots) as total
-FROM intervention i
-JOIN parlementaire p ON p.id = i.parlementaire_id
-WHERE i.type = \"commission\"
-GROUP BY i.date, renouveau"
-
-query data/hemicycle_renouveau "
-SELECT i.date, IF(url_ancien_cpc IS NULL, \"Nouveaux députés\", \"Députés réélus\") as renouveau, sum(i.nb_mots) as total
-FROM intervention i
-JOIN parlementaire p ON p.id = i.parlementaire_id
-WHERE i.type <> \"commission\"
-GROUP BY i.date, renouveau"
-
-bin/assemble_data.py > data/parole-deputes.csv
